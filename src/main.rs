0a8d768abf608c86d8e0f3c56338e865c71638cb
Zakaria Nacer <zak.nacer@hotmail.fr>
2024-12-02 10:36:36 +0100

    CLI command

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ pub mod day1;
 pub mod day2;
 use std::{collections::HashMap, sync::Arc};
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use day1::Day1;
 use day2::Day2;
 
@@ -16,8 +16,12 @@ pub trait DaySolutions {
 }
 #[derive(Debug, Parser)]
 struct Args {
-    #[arg(short, long, default_value_t = 2)]
-    day: usize,
+    #[command(subcommand)]
+    cmd: Commands,
+}
+#[derive(Debug, Subcommand, Clone)]
+enum Commands {
+    Day { day: usize },
 }
 pub fn get_day(day: usize) -> Option<Arc<dyn DaySolutions>> {
     let mut days: HashMap<usize, Arc<dyn DaySolutions>> = HashMap::new();
@@ -28,9 +32,12 @@ pub fn get_day(day: usize) -> Option<Arc<dyn DaySolutions>> {
 }
 fn main() {
     let args = Args::parse();
+    let day_value = match args.cmd {
+        Commands::Day { day } => day,
+    };
 
-    if let Some(day) = get_day(args.day) {
-        println!("Day {}:", args.day);
+    if let Some(day) = get_day(day_value) {
+        println!("Day {}:", day_value);
         println!("----PART 1----");
         println!("Example: {}", day.part1(Puzzle::Example));
         println!("Solution: {}", day.part1(Puzzle::Puzzle));
@@ -38,6 +45,6 @@ fn main() {
         println!("Example: {}", day.part2(Puzzle::Example));
         println!("Solution: {}", day.part2(Puzzle::Puzzle));
     } else {
-        println!("Day {} is not implemented yet.", args.day);
+        println!("Day {} is not implemented yet.", day_value);
     }
 }
